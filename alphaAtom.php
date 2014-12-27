ed87847ca1088a293936ac30642e76c6250958ab
Tester2009 <sulakecorporation1232@gmail.com>
2014-12-27 14:36:09 +0800

    first commit

---
+++ alphaAtom.php
@@ -0,0 +1,185 @@
+<?php
+/*
+* Proudly Coded by Muhammad Aliff Muazzam (Tester2009) .
+* Thanks to Mohd Shahril 96 for a little help
+* <3 Cat xD
+* https://www.facebook.com/Tester2009
+* https://github.com/alepcat1710
+* Feel free to use . Do not change copyright, mastah !
+*/
+/*
+The MIT License (MIT)
+
+Copyright (c) [2014] [Muhammad Aliff Muazzam]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+error_reporting(0); // disable error_reporting
+
+echo "\n" . "  alphaAtom 0.1 Copyright (c) 2014 Muhammad Aliff Muazzam December 27, 2014\n"; // copyright
+
+if (PHP_SAPI != "cli")  
+{
+	die("This php is built for command-line-interface. Not for web.");
+}
+
+if ( !isset ($argv[1]) || $argv[1]=="-help" || $argv[1]=="-h")
+{
+	// set here if not setup anything
+      die("
+            \r  This is how it works:
+            \r  php \"{$argv[0]}\" input_currency output_currency value
+            \r\n  Example: php \"{$argv[0]}\" SGD MYR 12
+");
+}
+// sources: https://gist.github.com/Min2liz/5728013
+$country_currency = array (
+            'ALL' => 'Albania Lek',
+            'AFN' => 'Afghanistan Afghani',
+            'ARS' => 'Argentina Peso',
+            'AWG' => 'Aruba Guilder',
+            'AUD' => 'Australia Dollar',
+            'AZN' => 'Azerbaijan New Manat',
+            'BSD' => 'Bahamas Dollar',
+            'BBD' => 'Barbados Dollar',
+            'BDT' => 'Bangladeshi taka',
+            'BYR' => 'Belarus Ruble',
+            'BZD' => 'Belize Dollar',
+            'BMD' => 'Bermuda Dollar',
+            'BOB' => 'Bolivia Boliviano',
+            'BAM' => 'Bosnia and Herzegovina Convertible Marka',
+            'BWP' => 'Botswana Pula',
+            'BGN' => 'Bulgaria Lev',
+            'BRL' => 'Brazil Real',
+            'BND' => 'Brunei Darussalam Dollar',
+            'KHR' => 'Cambodia Riel',
+            'CAD' => 'Canada Dollar',
+            'KYD' => 'Cayman Islands Dollar',
+            'CLP' => 'Chile Peso',
+            'CNY' => 'China Yuan Renminbi',
+            'COP' => 'Colombia Peso',
+            'CRC' => 'Costa Rica Colon',
+            'HRK' => 'Croatia Kuna',
+            'CUP' => 'Cuba Peso',
+            'CZK' => 'Czech Republic Koruna',
+            'DKK' => 'Denmark Krone',
+            'DOP' => 'Dominican Republic Peso',
+            'XCD' => 'East Caribbean Dollar',
+            'EGP' => 'Egypt Pound',
+            'SVC' => 'El Salvador Colon',
+            'EEK' => 'Estonia Kroon',
+            'EUR' => 'Euro Member Countries',
+            'FKP' => 'Falkland Islands (Malvinas) Pound',
+            'FJD' => 'Fiji Dollar',
+            'GHC' => 'Ghana Cedis',
+            'GIP' => 'Gibraltar Pound',
+            'GTQ' => 'Guatemala Quetzal',
+            'GGP' => 'Guernsey Pound',
+            'GYD' => 'Guyana Dollar',
+            'HNL' => 'Honduras Lempira',
+            'HKD' => 'Hong Kong Dollar',
+            'HUF' => 'Hungary Forint',
+            'ISK' => 'Iceland Krona',
+            'INR' => 'India Rupee',
+            'IDR' => 'Indonesia Rupiah',
+            'IRR' => 'Iran Rial',
+            'IMP' => 'Isle of Man Pound',
+            'ILS' => 'Israel Shekel',
+            'JMD' => 'Jamaica Dollar',
+            'JPY' => 'Japan Yen',
+            'JEP' => 'Jersey Pound',
+            'KZT' => 'Kazakhstan Tenge',
+            'KPW' => 'Korea (North) Won',
+            'KRW' => 'Korea (South) Won',
+            'KGS' => 'Kyrgyzstan Som',
+            'LAK' => 'Laos Kip',
+            'LVL' => 'Latvia Lat',
+            'LBP' => 'Lebanon Pound',
+            'LRD' => 'Liberia Dollar',
+            'LTL' => 'Lithuania Litas',
+            'MKD' => 'Macedonia Denar',
+            'MYR' => 'Malaysia Ringgit',
+            'MUR' => 'Mauritius Rupee',
+            'MXN' => 'Mexico Peso',
+            'MNT' => 'Mongolia Tughrik',
+            'MZN' => 'Mozambique Metical',
+            'NAD' => 'Namibia Dollar',
+            'NPR' => 'Nepal Rupee',
+            'ANG' => 'Netherlands Antilles Guilder',
+            'NZD' => 'New Zealand Dollar',
+            'NIO' => 'Nicaragua Cordoba',
+            'NGN' => 'Nigeria Naira',
+            'NOK' => 'Norway Krone',
+            'OMR' => 'Oman Rial',
+            'PKR' => 'Pakistan Rupee',
+            'PAB' => 'Panama Balboa',
+            'PYG' => 'Paraguay Guarani',
+            'PEN' => 'Peru Nuevo Sol',
+            'PHP' => 'Philippines Peso',
+            'PLN' => 'Poland Zloty',
+            'QAR' => 'Qatar Riyal',
+            'RON' => 'Romania New Leu',
+            'RUB' => 'Russia Ruble',
+            'SHP' => 'Saint Helena Pound',
+            'SAR' => 'Saudi Arabia Riyal',
+            'RSD' => 'Serbia Dinar',
+            'SCR' => 'Seychelles Rupee',
+            'SGD' => 'Singapore Dollar',
+            'SBD' => 'Solomon Islands Dollar',
+            'SOS' => 'Somalia Shilling',
+            'ZAR' => 'South Africa Rand',
+            'LKR' => 'Sri Lanka Rupee',
+            'SEK' => 'Sweden Krona',
+            'CHF' => 'Switzerland Franc',
+            'SRD' => 'Suriname Dollar',
+            'SYP' => 'Syria Pound',
+            'TWD' => 'Taiwan New Dollar',
+            'THB' => 'Thailand Baht',
+            'TTD' => 'Trinidad and Tobago Dollar',
+            'TRY' => 'Turkey Lira',
+            'TRL' => 'Turkey Lira',
+            'TVD' => 'Tuvalu Dollar',
+            'UAH' => 'Ukraine Hryvna',
+            'GBP' => 'United Kingdom Pound',
+            'USD' => 'United States Dollar',
+            'UYU' => 'Uruguay Peso',
+            'UZS' => 'Uzbekistan Som',
+            'VEF' => 'Venezuela Bolivar',
+            'VND' => 'Viet Nam Dong',
+            'YER' => 'Yemen Rial',
+            'ZWD' => 'Zimbabwe Dollar'
+        );
+
+// lets setup here.
+$from = strtoupper($argv[1]);
+$to = strtoupper($argv[2]); //"MYR";
+$quantity = $argv[3];
+$a = file_get_contents("http://rate-exchange.appspot.com/currency?from=$from&to=$to&q=$quantity");
+$b = json_decode($a);
+$c = $b->v; 
+$d = $b->rate;
+foreach($country_currency as $currency => $country)
+{
+      if ($from == $currency)
+      {
+            echo "\n  Rate for: 1 " .$currency. " ($country) equal to $to " .$d. "\n";
+            echo "  Conversion for $argv[3] $currency to MYR is: $to " .$c. "\n";
+      }
+}
